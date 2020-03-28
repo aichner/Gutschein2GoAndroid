b601c380aab188f86df1715ad4146cb5a74d867f
Katharina Leitner <leitnerkatharina02@gmail.com>
2020-03-28 20:40:16 +0100

    Create GUI and implement QR scanner
    Library has been implemented

--- Gutschein2Go/app/src/main/java/com/example/gutschein2go/MainActivity.java
+++ Gutschein2Go/app/src/main/java/com/example/gutschein2go/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.gutschein2go;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.zxing.integration.android.IntentIntegrator;
 
 public class MainActivity extends AppCompatActivity {
 
+    final Activity activity = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void ButtonQR_Click (View v) {
+        IntentIntegrator integrator = new IntentIntegrator(activity);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+        integrator.setPrompt("Scannen Sie den Gutschein QR-Code");
+        integrator.setCameraId(0);
+        integrator.setBeepEnabled(false);
+        integrator.initiateScan();
+    }
 }
+
+
