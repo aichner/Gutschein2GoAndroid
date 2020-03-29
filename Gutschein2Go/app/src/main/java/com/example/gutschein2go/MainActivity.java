86eecb01ade0e90429b191753d953c6e6dabd2d6
Katharina Leitner <leitnerkatharina02@gmail.com>
2020-03-29 17:22:55 +0200

    Finish App
    Functionality has been added to all buttons
    Some bugs have been fixed regarding the QR-Code scanner

--- Gutschein2Go/app/src/main/java/com/example/gutschein2go/MainActivity.java
+++ Gutschein2Go/app/src/main/java/com/example/gutschein2go/MainActivity.java
@@ -1,20 +1,31 @@
 package com.example.gutschein2go;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView textViewGueltigkeit;
+    ImageView imageView;
     final Activity activity = this;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textViewGueltigkeit = findViewById(R.id.textViewGueltigkeit);
+        imageView = findViewById(R.id.imageView);
+
     }
     public void ButtonQR_Click (View v) {
         IntentIntegrator integrator = new IntentIntegrator(activity);
@@ -23,6 +34,32 @@ public class MainActivity extends AppCompatActivity {
         integrator.setCameraId(0);
         integrator.setBeepEnabled(false);
         integrator.initiateScan();
+    }
+
+    public void ButtonCode_Click (View v) {
+        Intent i = new Intent(this, WebViewActivity.class);
+        this.startActivity(i);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result.getContents() != null) {
+            String url = result.getContents();
+            Intent i = new Intent(this, WebViewActivity.class);
+            i.putExtra("url", url);
+            this.startActivity(i);
+        }
+       else {
+            Intent i = new Intent(this, MainActivity.class);
+            this.startActivity(i);
+        }
     }
 }
 
--- Gutschein2Go/app/src/main/java/com/example/gutschein2go/WebViewActivity.java
+++ Gutschein2Go/app/src/main/java/com/example/gutschein2go/WebViewActivity.java
@@ -2,13 +2,69 @@ package com.example.gutschein2go;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class WebViewActivity extends AppCompatActivity {
 
+    WebView webView;
+    String url = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
+        webView = findViewById(R.id.WebView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url){
+                // do your handling codes here, which url is the requested url
+                // probably you need to open that url rather than redirect:
+                view.loadUrl(url);
+                return false; // then it is not handled by default action
+            }
+        });
+        Intent i = this.getIntent();
+        Bundle extras = i.getExtras();
+        if(extras != null) {
+            url = extras.getString("url");
+            if(Patterns.WEB_URL.matcher(url).matches()){
+                if(url.contains("g2g.at"))
+                {
+                    webView.loadUrl(url);
+                }
+                else {
+                    webView.loadUrl("https://www.g2g.at/verify/enter");
+                }
+            }
+            else {
+                Intent intent = new Intent(this, MainActivity.class);
+                this.startActivity(intent);
+            }
+
+        }
+        else {
+            webView.loadUrl("https://www.g2g.at/verify/enter");
+        }
+    }
+
+    public void ButtonBack_Click (View v) {
+        Intent i = new Intent(this, MainActivity.class);
+        this.startActivity(i);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack())
+            webView.goBack();
+        else {
+            super.onBackPressed();
+        }
     }
 }
